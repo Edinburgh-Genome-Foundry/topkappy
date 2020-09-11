41591b453348d6c17aa1167ef55e8fb82d753548
Peter Vegh <pvegh@posteo.uk>
2020-09-11 21:52:22 +0100

    License autodetection fix

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 import ez_setup
+
 ez_setup.use_setuptools()
 
 from setuptools import setup, find_packages
 
-exec(open('topkappy/version.py').read()) # loads __version__
+exec(open("topkappy/version.py").read())  # loads __version__
 
 setup(
-    name='topkappy',
+    name="topkappy",
     version=__version__,
-    author='Zulko',
-    url='https://github.com/Edinburgh-Genome-Foundry/topkappy',
-    description='Pythonic wrapper for the Kappa language (bio simulations)',
-    long_description=open('pypi-readme.rst').read(),
-    license='MIT',
+    author="Zulko",
+    url="https://github.com/Edinburgh-Genome-Foundry/topkappy",
+    description="Pythonic wrapper for the Kappa language (bio simulations)",
+    long_description=open("pypi-readme.rst").read(),
+    license="MIT",
     keywords="simulation biology modeling complex kappa binding",
-    packages=find_packages(exclude='docs'),
-    install_requires=['kappy', 'networkx', 'matplotlib', 'numpy',
-                      'termcolor'])
+    packages=find_packages(exclude="docs"),
+    install_requires=["kappy", "networkx", "matplotlib", "numpy", "termcolor"],
+)
